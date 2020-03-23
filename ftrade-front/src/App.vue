7d47aae1ddd400180a44809fae177dabc1fca956
littlehui <aod2011@163.com>
2020-03-23 15:08:33 +0800

    s

--- ftrade-front/src/App.vue
+++ ftrade-front/src/App.vue
@@ -4,11 +4,7 @@
       <el-container>
         <el-header height="80px">
           <div class="navbar-header" >
-<<<<<<< HEAD
             <a class="navbar-brand" href="/">FTRADE<span>-dev</span></a>
-=======
-            <a class="navbar-brand" href="/">FTRADE<span>1.0</span></a>
->>>>>>> aaaf858443cccacb5240b9c772f97a6d234cc660
           </div>
           <div class="navbar-menu">
             <el-menu :default-active="defaultTopMenuActive" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
