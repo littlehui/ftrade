240d91b492e5df18b73bcd0a20d056fe249c4b4f
littlehui <aod2011@163.com>
2020-03-23 15:30:06 +0800

    删除file-base

--- filebase-web/src/main/resources/public/static/js/15.ca1b8c87a4560a583b6f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([15],{F4Gm:function(l,e,u){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);e.default=n.exports}});
-//# sourceMappingURL=15.ca1b8c87a4560a583b6f.js.map
